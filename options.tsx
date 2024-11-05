f8df6da03a0c617759772633bcd3209aec717606
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-11-04 19:45:41 -0800

    still work in progress but it works

--- options.tsx
+++ options.tsx
@@ -1,28 +1,26 @@
 import { useEffect, useState } from "react"
 
+import { sendToBackground, sendToContentScript } from "@plasmohq/messaging"
 import { useStorage } from "@plasmohq/storage/hook"
 
 function OptionsIndex() {
   const [username, setUsername] = useStorage("username", "")
-  //   const storage = new Storage()
-  //   const [data, setData] = useState("")
-  setUsername("testing")
-  useEffect(() => {
-    const saveStorage = async () => {
-      //   const res = await storage.set("username", "foo")
-      //   console.log({ res })
-      //   const res2 = await storage.get("username")
-      //   console.log({ res2 })
-    }
-    saveStorage()
-  })
+  const [password, setPassword] = useStorage("password", "")
+
   return (
     <div>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <h2>This is the Option UI page!</h2>
-      {/* <input onChange={(e) => setData(e.target.value)} value={data} /> */}
+      <h1>Set your bsky login here</h1>
+      <input
+        placeholder="username"
+        onChange={async (e) => setUsername(e.target.value)}
+        value={username}
+      />
+      <input
+        type="password"
+        placeholder="app password"
+        onChange={async (e) => setPassword(e.target.value)}
+        value={password}></input>
+      <h2>Settings auto save</h2>
     </div>
   )
 }
--- popup.tsx
+++ popup.tsx
@@ -1,3 +1,4 @@
+import { AtpAgent } from "@atproto/api"
 import { useEffect, useState } from "react"
 
 import { sendToBackground, sendToContentScript } from "@plasmohq/messaging"
@@ -13,36 +14,75 @@ export async function grabTitle(url: string): Promise<string> {
 }
 
 function IndexPopup() {
-  const [username, setUsername] = useStorage("username")
-  const [data, setData] = useState("")
+  const agent = new AtpAgent({
+    service: "https://bsky.social"
+  })
+
+  const [username, setUsername] = useStorage("username", "")
+  const [password, setPassword] = useStorage("password", "")
+  const [title, setTitle] = useState("")
+  const [url, setUrl] = useState("")
   useEffect(() => {
     console.log("popup loaded")
-    console.log({ username })
-    const sendContent = async () => {
-      const resp2 = await sendToBackground({
-        name: "save",
-        body: {
-          username: "testing"
-        }
-      })
-      // const username = await storage.get("username")
-      // console.log({ username })
-      const resp = await sendToContentScript({ name: "url" })
-      console.log({ resp })
-      const title = await grabTitle(resp)
+
+    console.log("inside async")
+    const doAsync = async () => {
+      const url = await sendToContentScript({ name: "url" })
+      console.log({ url })
+      setUrl(url)
+      console.log({ url })
+      const title = await grabTitle(url)
       console.log({ title })
-      setData(title)
+      setTitle(title)
     }
-    sendContent()
+    doAsync()
   }, [])
-  return (
+
+  const hasUsername = (
     <div
       style={{
         padding: 16
       }}>
       <div>Username: {username}</div>
-      <div>Title: {data}</div>
-      <a href="">Save bookmark</a>
+      <div>Title: {title}</div>
+      <button
+        onClick={async (e) => {
+          console.log(username, password)
+
+          const login = await agent.login({
+            identifier: username,
+            password
+          })
+          console.log({ login })
+          console.log({ url })
+          const res1 = await agent.com.atproto.repo.createRecord({
+            did: agent.did,
+            repo: agent.did,
+            collection: "fyi.unravel.frontpage.post",
+            record: {
+              $type: "fyi.unravel.frontpage.post",
+              title,
+              url,
+              createdAt: new Date().toISOString()
+            }
+          })
+          console.log({ res1 })
+        }}>
+        Save bookmark
+      </button>
+    </div>
+  )
+
+  const noUsername = (
+    <div>
+      <a href="/options.html" target="_blank">
+        click here to set login info
+      </a>
+    </div>
+  )
+  return (
+    <div>
+      {username && hasUsername} {!username && noUsername}
     </div>
   )
 }
